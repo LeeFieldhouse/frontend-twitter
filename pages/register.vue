ea2ce300215a8e1fe712af16bd4748411cc63de3
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-16 09:46:00 +0100

    Update register.vue

--- pages/register.vue
+++ pages/register.vue
@@ -10,8 +10,11 @@
               <v-card-text>
                 <v-form>
                   <v-text-field v-model="data.name" prepend-icon="person" name="name" label="Name" type="text"></v-text-field>
+                  <p class="error-msg">Error message</p>
                   <v-text-field v-model="data.email" prepend-icon="alternate_email" name="email" label="Email" type="text"></v-text-field>
+                  <p class="error-msg">Error message</p>
                   <v-text-field v-model="data.password" id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                  <p class="error-msg">Error message</p>
                 </v-form>
               </v-card-text>
               <v-card-actions>
