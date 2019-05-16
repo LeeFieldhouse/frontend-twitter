15169b7958abf460733c4612d39def3ef1f96bc7
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-16 09:32:36 +0100

    login & register page

---
+++ components/navbar.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+        <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      fixed
+      app
+      color="blue"
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      fixed
+      app
+      color="white--text"
+    >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+
+      <v-spacer />
+      <img class="nav-logo" src="https://static1.squarespace.com/static/57c5e40db3db2be4194b8bdb/t/5873cac617bffc8caed01666/1483983697650/twitter-logo.png" alt="">
+      <v-spacer />
+
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      miniVariant: true,
+      items: [
+        {
+          icon: 'perm_identity',
+          title: 'Login',
+          to: '/login'
+        },
+        {
+          icon: 'person_add',
+          title: 'Register',
+          to: '/register'
+        }
+      ],
+
+    }
+
+  }
+
+}
+</script>
+
+<style>
+nav {
+  background: #659FCB!important;
+}
+
+.nav-logo {
+  max-height: 100%;
+  max-width: 100%;
+}
+
+.v-toolbar__content .v-btn--icon {
+  color: white!important;
+}
+
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md6>
+            <v-card class="elevation-4">
+              <v-toolbar dark>
+                <v-toolbar-title>Register form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field v-model="data.name" prepend-icon="person" name="name" label="Name" type="text"></v-text-field>
+                  <v-text-field v-model="data.email" prepend-icon="alternate_email" name="email" label="Email" type="text"></v-text-field>
+                  <v-text-field v-model="data.password" id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {
+        name: null,
+        email: null,
+        password: null,
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
